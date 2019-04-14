35e660d951a3bee63744930a4bb24a95e70f8c4e
yichuanshuzi <chenboshuo.hi@163.com>
2019-04-14 13:58:25 +0800

    12.3.3 创建设置类
    每次给游戏添加新功能时, 
    通常要应用一些新的设置.模块setting包含一个Setting类将所有设置存储在同一个地方,以免在代码中到处添加设置. 这样, 
    我们只需要传递一个设置对象. 另外, 这让函数调用更简单,在项目增大时修改外观更容易: 
    修改游戏只需要修改setting中的一些值,而无需查找分布在项目中的各种设置.

--- pygame/alien_invasion/alien_invason.py
+++ pygame/alien_invasion/alien_invason.py
@@ -1,15 +1,16 @@
 import sys
 
 import pygame
+from settings import Setting
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init() # 初始化背景设置,让pygame能正常工作
-    screen =  pygame.display.set_mode((1200, 800)) # 创建(1200, 800)的窗口
+    ai_settings = Setting()
+    screen =  pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    ) # 创建(1200, 800)的窗口
     pygame.display.set_caption("Alien Invasion")
-
-    # 设置游戏背景色
-    bg_color = (230, 230, 230)
 
     # 开始游戏主循环
     while True:
@@ -20,7 +21,7 @@ def run_game():
                 sys.exit()
 
         # 每次循环都重新绘制屏幕
-        screen.fill(bg_color) # 这个方法只接受一种实参: 一种颜色
+        screen.fill(ai_settings.bg_color) # 这个方法只接受一种实参: 一种颜色
 
         # 让最近的屏幕可见
         pygame.display.flip()
