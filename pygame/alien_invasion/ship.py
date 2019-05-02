8377c7be2f4692b9fecd90db30c080cc68a59f2c
yichuanshuzi <chenboshuo.hi@163.com>
2019-05-02 10:37:39 +0800

    12.6.4 调整飞船速度 当前, 执行while循环时, 飞船最多移动1像素, 但我们可以添加设置控制移动距离:
    我们设置ship_speed_factor 的值为1.5, 可以加快游戏节奏, 然而, rect的`centerx`等属性只能存储整数值, 
    因此还要进行修改,(创建一个ship.center存储 float 的中间变量再取整)
    
    同时在ship的`__init__()`的形参列表添加了`ai_settings`让飞船可以快速读取设置

--- pygame/alien_invasion/ship.py
+++ pygame/alien_invasion/ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings,screen):
         """初始化飞船并设置初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船并获取其外界矩形
         self.imag = pygame.image.load('images/ship.bmp') # 函数返回一个表示飞船外形的surface
@@ -15,6 +16,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # 在飞船属性中存储小数值
+        self.center = float(self.rect.centerx)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
@@ -22,9 +26,12 @@ class Ship():
     def update(self):
         '''根据移动标志调整飞船位置'''
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 根据self.center(小数) 更新rect 对象(整数部分)
+        self.rect.centerx = self.center
 
     def biltme(self):
         '''在指定位置绘制飞船'''
