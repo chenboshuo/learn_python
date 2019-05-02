8e2aa07a52795311efa93d78a8ac74d6d1c4fc33
yichuanshuzi <chenboshuo.hi@163.com>
2019-05-02 08:02:35 +0800

    12.5.1 函数check_events()

--- pygame/alien_invasion/alien_invason.py
+++ pygame/alien_invasion/alien_invason.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Setting
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -21,9 +22,7 @@ def run_game():
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # 每次循环都重新绘制屏幕
         screen.fill(ai_settings.bg_color) # 这个方法只接受一种实参: 一种颜色
