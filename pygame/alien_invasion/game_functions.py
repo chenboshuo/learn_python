e8617ef84edb19adb684047e3b191ec4fb02a171
yichuanshuzi <chenboshuo.hi@163.com>
2019-05-12 09:21:38 +0800

    12.8.1-4 添加子弹设置 创建`Bullet`类 将子弹存到编组中 开火
    ### 12.8.1 添加子弹设置
    更新`settings.py`在__init__()末尾存储设置
    
    ### 12.8.2 创建`Bullet`类
    
    通过使用精灵, 可以将游戏中相关的元素编组, 进而同时操作编组中的所有元素.
    
    ### 12.8.3 将子弹存到编组中
    
    我们在`alien_invason.py`中创建一个编组(Group), 用于存储所有有效子弹, 以便能够管理发射出去的所有子弹. 
    这个编组是pygame.sprite.Group 类的实例; pygame.sprite.Group 类似于列表, 
    但提供了有助于游戏开发的额外功能. 在主循环中, 我们将使用这个编组在屏幕上绘制子弹, 以更新每颗子弹的位置
    
    ### 12.8.4 开火
    修改`check_keydown_events()` 以便玩家按空格的时候发射一颗子弹, 
    无需修改`check_keyup_events()`因为玩家松开空格的时候什么都不会发生. 我们还要修改update_screen(), 
    确保在调用flip()的时候重绘每颗子弹

--- pygame/alien_invasion/game_functions.py
+++ pygame/alien_invasion/game_functions.py
@@ -2,7 +2,9 @@ import sys
 
 import pygame
 
-def check_keydown_events(event, ship):
+from bullet import Bullet
+
+def check_keydown_events(event,ai_settings, screen,ship, bullets):
     '''响应按键'''
     # 读取属性event.key, 检查按下的键是否为右箭头( pygame.K_RIGHT)
     if event.key == pygame.K_RIGHT:
@@ -10,6 +12,10 @@ def check_keydown_events(event, ship):
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # 新建一颗子弹, 并将其加入到编组bullets 中
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -17,21 +23,24 @@ def check_keyup_events(event, ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     '''响应按键和鼠标事件'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event,ai_settings, screen,ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     '''更新屏幕上的图像,并切换到新屏幕'''
     # 每次循环都重新绘制屏幕
     screen.fill(ai_settings.bg_color)  # 这个方法只接受一种实参: 一种颜色
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.biltme()
 
     # 让最近的屏幕可见
