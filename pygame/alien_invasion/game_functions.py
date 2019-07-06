ded65ee27083dea3448cabf68b8190527fd33fac
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-06 08:34:18 +0800

    3.5.1 检测子弹与外星人相撞

--- pygame/alien_invasion/game_functions.py
+++ pygame/alien_invasion/game_functions.py
@@ -56,10 +56,19 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     # 让最近的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
-    '''更新子弹位置, 删除已经消失的子弹'''
+def update_bullets(aliens, bullets):
+    '''更新子弹位置, 删除已经消失的子弹    '''
     # 更新子弹位置
     bullets.update()
+    # 检查是否有子弹击中了外星人
+    # 如果这样, 就删除子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    # 新增的这行代码遍历编组bullets中的每颗子弹,再遍历编组aliens中的每个外星人.
+    # 每当有子弹和外星人的rect重叠时, groupcollide()就在它返回的字典中添加一个键-值对.
+    # 两个实参True告诉pygame要删除发生碰撞的子弹和外星人.
+    # (要模拟能穿行到屏幕顶端的高能子弹--消灭它击中的每一个外星人,可将第一个布尔参数设为False.
+    #  这样被他击中的外星人将消失,所有子弹始终有效, 直到抵达屏幕顶端后消失.)
+
 
     # 删除已消失的子弹(不应从列表或编组中删除条目)
     for bullet in bullets.copy():
