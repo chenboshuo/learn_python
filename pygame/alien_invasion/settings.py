58bbfc7c9d064324c3a9a9cda87b0496253467cd
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-11 11:29:26 +0800

    13.5.2 为测试创建大子弹

--- pygame/alien_invasion/settings.py
+++ pygame/alien_invasion/settings.py
@@ -10,7 +10,7 @@ class Setting():
 
         # 子弹设置
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60 # 深灰色子弹
         self.bullets_allowed = 3
