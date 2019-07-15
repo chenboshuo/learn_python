ba8d68750b429c53a26f683951fd7701ba60de61
yichuanshuzi <chenboshuo.hi@163.com>
2019-07-15 08:59:50 +0800

    13.5.2 提高子弹速度

--- pygame/alien_invasion/settings.py
+++ pygame/alien_invasion/settings.py
@@ -9,7 +9,7 @@ class Setting():
         self.bg_color = (230, 230, 230)
 
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 2
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60 # 深灰色子弹
